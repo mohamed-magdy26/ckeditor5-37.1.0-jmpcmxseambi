f9f4afbf90818594d48ee2548c1fcd91f5aca9b8
mohamed magdy <mohamed.magdy0598@gmail.com>
2023-05-12 06:09:38 +0300

    rebuilding using online builder to add more features

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,63 +4,80 @@
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageInsert from '@ckeditor/ckeditor5-image/src/imageinsert.js';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SelectAll from '@ckeditor/ckeditor5-select-all/src/selectall.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Style from '@ckeditor/ckeditor5-style/src/style.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Alignment,
-	Autoformat,
 	BlockQuote,
 	Bold,
-	CloudServices,
+	Code,
+	CodeBlock,
 	Essentials,
+	FontColor,
+	FontFamily,
+	FontSize,
+	GeneralHtmlSupport,
 	Heading,
+	HorizontalLine,
 	Image,
 	ImageCaption,
-	ImageStyle,
+	ImageInsert,
 	ImageToolbar,
 	ImageUpload,
 	Indent,
+	IndentBlock,
 	Italic,
 	Link,
+	LinkImage,
 	List,
-	MediaEmbed,
+	ListProperties,
 	Paragraph,
-	PasteFromOffice,
-	Table,
-	TableToolbar,
-	TextTransformation
+	RemoveFormat,
+	SelectAll,
+	Strikethrough,
+	Style,
+	Subscript,
+	Superscript,
+	Underline
 ];
 
 // Editor configuration.
 Editor.defaultConfig = {
 	toolbar: {
 		items: [
-			'alignment',
-			'outdent',
-			'indent',
 			'heading',
 			'|',
 			'bold',
@@ -69,12 +86,28 @@ Editor.defaultConfig = {
 			'bulletedList',
 			'numberedList',
 			'|',
+			'outdent',
+			'indent',
+			'|',
 			'imageUpload',
 			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
 			'undo',
-			'redo'
+			'redo',
+			'alignment',
+			'codeBlock',
+			'code',
+			'fontColor',
+			'fontSize',
+			'fontFamily',
+			'imageInsert',
+			'horizontalLine',
+			'removeFormat',
+			'selectAll',
+			'strikethrough',
+			'style',
+			'underline',
+			'superscript',
+			'subscript'
 		]
 	},
 	language: 'en',
@@ -82,16 +115,7 @@ Editor.defaultConfig = {
 		toolbar: [
 			'imageTextAlternative',
 			'toggleImageCaption',
-			'imageStyle:inline',
-			'imageStyle:block',
-			'imageStyle:side'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
+			'linkImage'
 		]
 	}
 };
